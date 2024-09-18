985fd57255455263c49da5ac8bef8bcdad6e4193
Messansio <andreamessanaj@gmail.com>
2024-09-18 15:33:01 +0200

    file were not working properly

--- ascii_art.py
+++ ascii_art.py
@@ -1,51 +1,51 @@
 # Install the required libraries: pip install pillow click
-# How to run the script: python ascii_art.py <imagePath> --width <width>
+# How to run the script: python ascii_art.py <image_path> --width <width>
 # made by Messansio
 import click
 from PIL import Image
 
 ASCII_CHARS = "@%#*+=-:. "
 
-def resizeImage(image, newWidth=100):
+def resize_image(image, new_width=100):
     width, height = image.size
-    aspectRatio = height / width
-    newHeight = int(aspectRatio * newWidth)
-    resizedImage = image.resize((newWidth, newHeight))
-    return resizedImage
+    aspect_ratio = height / width
+    new_height = int(aspect_ratio * new_width)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
 
 def grayify(image):
-    grayscaleImage = image.convert("L")
-    return grayscaleImage
+    grayscale_image = image.convert("L")
+    return grayscale_image
 
-def pixelsToAscii(image):
+def pixels_to_ascii(image):
     pixels = image.getdata()
-    asciiStr = "".join([ASCII_CHARS[pixel // 32] for pixel in pixels])
-    return asciiStr
+    ascii_str = "".join([ASCII_CHARS[pixel // 32] for pixel in pixels])
+    return ascii_str
 
-def imgToAscii(imagePath, newWidth=100):
+def convert_image_to_ascii(image_path, new_width=100):
     try:
-        image = Image.open(imagePath)
+        image = Image.open(image_path)
     except Exception as e:
-        print(f"Unable to open image file {imagePath}.")
+        print(f"Unable to open image file {image_path}.")
         print(e)
         return
 
-    image = resizeImage(image, newWidth)
+    image = resize_image(image, new_width)
     image = grayify(image)
-    asciiStr = pixelsToAscii(image)
+    ascii_str = pixels_to_ascii(image)
 
-    imgWidth = image.width
-    asciiStrLen = len(asciiStr)
-    asciiImg = "\n".join([asciiStr[index:(index + imgWidth)] for index in range(0, asciiStrLen, imgWidth)])
+    img_width = image.width
+    ascii_str_len = len(ascii_str)
+    ascii_img = "\n".join([ascii_str[index:(index + img_width)] for index in range(0, ascii_str_len, img_width)])
 
-    print(asciiImg)
+    print(ascii_img)
 
 @click.command()
-@click.argument('imagePath')
+@click.argument('image_path')
 @click.option('--width', default=100, help='Width of the ASCII art')
-def cli(imagePath, width):
+def cli(image_path, width):
     """Convert an image to ASCII art."""
-    imgToAscii(imagePath, width)
+    convert_image_to_ascii(image_path, width)
 
 if __name__ == "__main__":
     cli()
